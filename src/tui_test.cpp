1882ff04c5b981e4936616a7aaf17f4deaba85df
Gabriel Nieves Hub <159567463+GabrielNievesRamirez@users.noreply.github.com>
2024-04-19 01:59:49 +0000

    Version 2

--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -1,34 +1,52 @@
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/color.hpp>
 #include <iostream>
 #include <string>
+#include <thread>
+#include <experimental/random>
 
 using namespace std;
 using namespace ftxui;
 
-
 int main(int argc, char const *argv[])
 {
-    const string texto = "Hola Mundo";
-    Element textElement = text(texto);
+  int fotograma = 0;
 
-    Element dibujo = hbox(textElement | border);
+  string reset;
 
-    Dimensions Alto = Dimension::Fixed(10);
-    Dimensions Ancho = Dimension::Fixed(10);
+  while (true)
+  {
+   fotograma++;
 
-
-
-    Screen pantalla = Screen::Create(Ancho,Alto);
-
-    Render(pantalla,dibujo);
-    
-    pantalla.Print();
-    cout<<endl;
-    
+   int r = std::experimental::randint(0, 255);
+   int g = std::experimental::randint(0, 255);
+   int b = std::experimental::randint(0, 255);
 
 
 
-    
-    return 0;
+   Element personaje = spinner(21,fotograma);
+   Decorator colorFondo = bgcolor(Color::RGB(r,g,b));
+   Decorator colorTexto = color(Color::RGB(r,g,b));
+   Element dibujo = border({
+      hbox(personaje) 
+  })| colorFondo | colorTexto;
+
+   Dimensions Alto = Dimension::Fixed(10);
+   Dimensions Ancho = Dimension::Full();
+   
+   Screen pantalla = Screen::Create(Ancho, Alto);
+
+   Render(pantalla,dibujo);
+   
+   pantalla.Print();
+   reset = pantalla.ResetPosition();
+   cout << reset;
+   this_thread::sleep_for(0.1s);
+  }
+  
+   return 0;
+  
+
 }
+
