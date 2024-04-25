8a9aaaf370c1c491f4d168d56ebfc81f3a7cef47
Gabriel Nieves Hub <159567463+GabrielNievesRamirez@users.noreply.github.com>
2024-04-25 02:04:26 +0000

    version 3

---
+++ include/pintor/Dibujo.hpp
@@ -0,0 +1,29 @@
+#include<string>
+#include<list>
+#include <vector.hpp>
+
+using namespace std;
+
+
+class Dibujo
+{
+private:
+    list<string> lineas;
+    vector posicion;
+    vector velocidad;
+public:
+    Dibujo() {}
+    ~Dibujo() {}
+    void AgregarLinea(string linea){
+        this -> lineas.push_back(linea);
+        
+    }
+    list<string> ObtenerLineas(){
+        return this->lineas;
+        
+    }
+};
+
+
+
+
---
+++ include/pintor/vector.hpp
@@ -0,0 +1,12 @@
+class Vector
+{:
+    
+    private:
+        int posX;
+        int posY;
+
+    public:
+        vector(){}
+        vector(){}
+
+;}
